584ca3e707b5b192e2021e8ee42e2169f03fb119
SteveRivSan <15101187@ue.edu.pe>
2023-12-02 03:14:08 -0500

    Desarrollo de Activity Soporte

---
+++ app/src/main/java/com/example/proyectofinalgrupo4_v21fin/ComentTiendaActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinalgrupo4_v21fin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComentTiendaActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coment_tienda2)
+    }
+}
